261d90cccb0c8e070064fc30c22b1bef2c139f1e
ArtificialAmateur <20297606+ArtificialAmateur@users.noreply.github.com>
2017-04-17 11:01:30 -0700

    eclipse compatible

---
+++ src/anem.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : anim.cpp
+// Author      : @ArtificialAmateur @alefir
+// Version     : 0.01
+// Copyright   : lol no
+// Description : Automatic Network Intrusion Monitor
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
