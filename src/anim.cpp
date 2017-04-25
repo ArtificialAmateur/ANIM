f3b6515748a3c1d8273778fdac387bf27eeeefd3
alefir <zmudd@san.rr.com>
2017-04-25 09:22:59 -0700

    Remove class structure so it sucks less Also started a semi-functional option handler

--- src/anim.cpp
+++ src/anim.cpp
@@ -7,85 +7,124 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string>
+
 using namespace std;
 
-class Anim {
+const char* version = "Anim v0.0.1";
+static int launcher() {
+	const char* jackal =
+			"    ___   ________   ________   ___  __     ________   ___          \n"
+					"   |\\  \\ |\\   __  \\ |\\   ____\\ |\\  \\|\\  \\  |\\   __  \\ |\\  \\         \n"
+					"   \\ \\  \\\\ \\  \\|\\  \\\\ \\  \\___| \\ \\  \\/  /|_\\ \\  \\|\\  \\\\ \\  \\        \n"
+					" __ \\ \\  \\\\ \\   __  \\\\ \\  \\     \\ \\   ___  \\\\ \\   __  \\\\ \\  \\       \n"
+					"|\\  \\\\_\\  \\\\ \\  \\ \\  \\\\ \\  \\____ \\ \\  \\\\ \\  \\\\ \\  \\ \\  \\\\ \\  \\____  \n"
+					"\\ \\________\\\\ \\__\\ \\__\\\\ \\_______\\\\ \\__\\\\ \\__\\\\ \\__\\ \\__\\\\ \\_______\\\n"
+					" \\|________| \\|__|\\|__| \\|_______| \\|__| \\|__| \\|__|\\|__| \\|_______|\n"
+					"                                                                    \n"
+					"          .                                                      .\n"
+					"        .n                   .                 .                  n.\n"
+					"  .   .dP                  dP                   9b                 9b.    .\n"
+					" 4    qXb         .   dX                     Xb   .        dXp     t\n"
+					"dX.    9Xb  .dXb    __                         __    dXb.     dXP     .Xb\n"
+					"9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP\n"
+					" 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP\n"
+					"  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'\n"
+					"    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'\n"
+					"        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~\n"
+					"                        )b.  .dbo.dP'`v'`9b.odb.  .dX(\n"
+					"                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.\n"
+					"                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb\n"
+					"                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb\n"
+					"                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP\n"
+					"                     `'      9XXXXXX(   )XXXXXXP      `'\n"
+					"                              XXXX X.`v'.X XXXX\n"
+					"                              XP^X'`b   d'`X^XX\n"
+					"                              X. 9  `   '  P )X\n"
+					"                              `b  `       '  d'\n"
+					"                               `             '";
+	cout << jackal;
+	return 0;
+}
 
-public:
-	const char* version = "Anim v0.0.1";
-	static int launcher() {
-		const char* jackal =
-				"    ___   ________   ________   ___  __     ________   ___          \n"
-						"   |\\  \\ |\\   __  \\ |\\   ____\\ |\\  \\|\\  \\  |\\   __  \\ |\\  \\         \n"
-						"   \\ \\  \\\\ \\  \\|\\  \\\\ \\  \\___| \\ \\  \\/  /|_\\ \\  \\|\\  \\\\ \\  \\        \n"
-						" __ \\ \\  \\\\ \\   __  \\\\ \\  \\     \\ \\   ___  \\\\ \\   __  \\\\ \\  \\       \n"
-						"|\\  \\\\_\\  \\\\ \\  \\ \\  \\\\ \\  \\____ \\ \\  \\\\ \\  \\\\ \\  \\ \\  \\\\ \\  \\____  \n"
-						"\\ \\________\\\\ \\__\\ \\__\\\\ \\_______\\\\ \\__\\\\ \\__\\\\ \\__\\ \\__\\\\ \\_______\\\n"
-						" \\|________| \\|__|\\|__| \\|_______| \\|__| \\|__| \\|__|\\|__| \\|_______|\n"
-						"                                                                    \n"
-						"          .                                                      .\n"
-						"        .n                   .                 .                  n.\n"
-						"  .   .dP                  dP                   9b                 9b.    .\n"
-						" 4    qXb         .   dX                     Xb   .        dXp     t\n"
-						"dX.    9Xb  .dXb    __                         __    dXb.     dXP     .Xb\n"
-						"9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP\n"
-						" 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP\n"
-						"  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'\n"
-						"    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'\n"
-						"        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~\n"
-						"                        )b.  .dbo.dP'`v'`9b.odb.  .dX(\n"
-						"                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.\n"
-						"                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb\n"
-						"                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb\n"
-						"                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP\n"
-						"                     `'      9XXXXXX(   )XXXXXXP      `'\n"
-						"                              XXXX X.`v'.X XXXX\n"
-						"                              XP^X'`b   d'`X^XX\n"
-						"                              X. 9  `   '  P )X\n"
-						"                              `b  `       '  d'\n"
-						"                               `             '";
-		std::cout << jackal;
-		return 0;
-	}
+static int help() {
+	const char* help =
+			"Usage: anim [OPTION]\n"
+					"Automatic Network Intrusion Monitor\n"
+					"\n"
+					"\t-m, --module [MODULE] \tRuns the module of name or number specified\n"
+					"\t-l, --list\t\tList installed modules\n"
+					"\t-v, --version\t\tDisplay currently installed version\n"
+					"\t-h, --help\t\tDisplay this help text";
+	cout << help;
+	return 0;
+}
 
-	static int help() {
-		const char* help =
-				"Usage: anim [OPTION]\n"
-						"Automatic Network Intrusion Monitor\n"
-						"\n"
-						"\t-m, --module [MODULE] \tRuns the module of name or number specified\n"
-						"\t-l, --list\t\tList installed modules\n"
-						"\t-v, --version\t\tDisplay currently installed version\n"
-						"\t-h, --help\t\tDisplay this help text";
-		std::cout << help;
-		return 0;
-	}
-};
+int getdir (string dir, vector<string> &files)
+{
+    DIR *dp;
+    struct dirent *dirp;
+    if((dp  = opendir(dir.c_str())) == NULL) {
+        cout << "Error(" << errno << ") opening " << dir << endl;
+        return errno;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        files.push_back(string(dirp->d_name));
+    }
+    closedir(dp);
+    return 0;
+}
+
+static int list_modules() {
+	string dir = string(".");
+    vector<string> files = vector<string>();
+
+    getdir(dir,files);
+
+    for (unsigned int i = 0;i < files.size();i++) {
+        cout << files[i] << endl;
+    }
+    return 0;
+}
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		Anim::help();
+		help();
 		return 1;
 	}
-	switch (argv[1]) {
-	case "-m" || "--module":
+	// C++ Doesn't allow strings in switches. I'm sorry.
+	if ( argv[1] == "-m" || argv[1] == "--module") {
 		if (argc < 3) {
-			std::cout << "Usage: anim -m [MODULE]";
+			cout << "Usage: anim -m [MODULE]";
 			return 1;
-		} else
-			Anim::run_module(argv[2]);
-		break;
-	case "-l" || "--list":
-		Anim::list_modules();
-		return 0;
-	case "-v" || "--version":
-		std::cout << version;
-		return 0;
-	case "-h" || "--help":
-		Anim::help();
-		return 0;
-	default:
-		Anim::help();
+		} else {
+			//run_module(argv[2]);
+			cout << "Pretend a module is being run";
+		}
 		return 1;
 	}
+	if ( argv[1] == "-l" || argv[1] == "--list") {
+		list_modules();
+		return 0;
+	}
+	if ( argv[1] == "-v" || argv[1] == "--version") {
+		cout << version;
+		return 0;
+	}
+	if ( argv[1] == "-h" || argv[1] == "--help") {
+		help();
+		return 0;
+	}
+	else {
+		//help();
+		cout << argv[1];
+		cout << "buffer";
+		return 1;
+	}
+	return 2;
 }
