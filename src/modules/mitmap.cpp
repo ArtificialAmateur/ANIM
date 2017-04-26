6f2945efaa5198ef16fb4ab3caa0274f89b29b50
alefir <zmudd@san.rr.com>
2017-04-25 20:13:31 -0700

    Add base mitmap.cpp

---
+++ src/modules/mitmap.cpp
@@ -0,0 +1,11 @@
+//============================================================================
+// Name        : mitmap.cpp
+// Author      : @ArtificialAmateur @alefir
+// Version     : 0.0.1a
+// Copyright   : dont steal our things
+// Description : Man In The Middle Access Point
+//============================================================================
+
+int main( int argc, char* argv[] ) {
+	return 0;
+}
