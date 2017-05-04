d4426a712b9285722bc93b21c83e9c6796a3835a
alefir <zmudd@san.rr.com>
2017-05-03 20:23:50 -0700

    Some partial progress on mitmap

--- src/modules/mitmap.cpp
+++ src/modules/mitmap.cpp
@@ -6,6 +6,51 @@
 // Description : Man In The Middle Access Point
 //============================================================================
 
-int main( int argc, char* argv[] ) {
+#include <iostream>
+#include <unistd.h>
+#include <vector>
+#include <sstream>
+#include <string>
+
+std::string char_to_string(char val) {
+	std::stringstream ss;
+	std::string s;
+	ss << val;
+	ss >> s;
+	return s;	
+}
+
+std::vector<std::string> split(const std::string &text, char* sep) {
+  std::vector<std::string> tokens;
+  std::size_t start = 0, end = 0;
+  while ((end = text.find(sep, start)) != std::string::npos) {
+    tokens.push_back(text.substr(start, end - start));
+    start = end + 1;
+  }
+  tokens.push_back(text.substr(start));
+  return tokens;
+}
+
+char run_command(char* command) {
+	FILE *fpipe;
+    char c = 0;
+
+    if(0 == (fpipe = (FILE*)popen(command, "r"))) {
+        perror("popen() failed.");
+        exit(1);
+    }
+    while (fread(&c, sizeof c, 1, fpipe)) {
+        return c;
+    }
+    pclose(fpipe);
+    return c;
+}
+
+int main(int argc, char* argv[]) {
+	char output[] = run_command("uname -a");
+	std::string uname = output;
+	std::cout << uname << std::endl;
+	std::string kernel = split(uname, " ")[2];
+	std::cout << kernel << std::endl;
 	return 0;
 }
